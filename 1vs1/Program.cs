8722188f0d7ef2fe4e2c1e1f757184bfc7fe0f16
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-09-21 10:56:22 +0200

    Mmm

--- 1vs1/Program.cs
+++ 1vs1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// SLUMP
+Random generator = new Random();
+
+int slumptal = generator.Next(101);
+
+Console.WriteLine(slumptal);
+
+//WHILE-LOOP
+
+int tal = 101;
+
+while (tal > 0)
+{
+    Console.WriteLine(tal);
+    tal--;
+}
+
+Console.ReadLine(You are FINISHED);
+
+Console.ReadLine();
