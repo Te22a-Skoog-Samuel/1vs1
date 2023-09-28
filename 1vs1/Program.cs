b7d6b8db1f65a0a45792cc62a7bd91d8f819b100
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-09-28 11:02:14 +0200

    ...

--- 1vs1/Program.cs
+++ 1vs1/Program.cs
@@ -1,12 +1,24 @@
 ﻿string HjälteNamn ="Hjälte";
 string SkurkNamn ="Skurk";
 
-int HjältesHP =100;
-int SkurkensHP =100;
+int HjältesHP =101;
+int SkurkensHP =101;
 
 Random generator = new Random();
 
 while (HjältesHP > 0 && SkurkensHP > 0)
+{
+Console.WriteLine("Next round, FIGHT!)";
+Console.WriteLine($"{HjälteNamn}: {HjältesHP}  {SkurkNamn}: {SkurkensHP}/n");
+
+int HjältenSkada = generator.Next(50);
+SkurkensHP -= HjältenSkada;
+SkurkensHP = Math.Max(0, SkurkensHP);
+Console.WriteLine($"{HjälteNamn} gör {HjältenSkada} skada på {SkurkNamn}");
+
+
+}
+
 
 int slumptal = generator.Next(101);
 
@@ -14,6 +26,6 @@ Console.WriteLine(slumptal);
 
 
 
-Console.WriteLine("You are FINISHED");
+Console.WriteLine("Destructive FINISH");
 
 Console.ReadLine();
